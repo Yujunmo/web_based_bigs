86526c6dd4f8fb6caa4df8d641f9805b7714c68b
Yujunmo <45279586+Yujunmo@users.noreply.github.com>
2025-10-01 15:00:00 +0900

    Update 펀드성과분석.py

--- F_성과분석/펀드성과분석.py
+++ F_성과분석/펀드성과분석.py
@@ -5,54 +5,7 @@ import plotly.express as px
 import yfinance as yf
 import openai
 from common.validation import date_validation
-
-# 공통으로 밀어넣어야함 ( 최대한 렌더링과 로직을 나눈다. )
-def cal_strn_date(strn_date:datetime, end_date:datetime, selected_columns) -> dict[str,str]:
-
-    strn_dates = {}
-    for col in selected_columns:
-        if col == '조회기간':
-            strn_dates[col+"("+strn_date.strftime('%Y-%m-%d')+")"] = strn_date.strftime('%Y-%m-%d')
-            pass # 조회기간 개선 필요
-        elif col == '1일':
-            strn_dates[col+"("+(end_date - timedelta(days=1)).strftime('%Y-%m-%d')+")"] = (end_date - timedelta(days=1)).strftime('%Y-%m-%d')
-        elif col == '1주':
-            strn_dates[col+"("+(end_date - timedelta(weeks=1)).strftime('%Y-%m-%d')+")"] = (end_date - timedelta(weeks=1)).strftime('%Y-%m-%d')
-        elif col == '1개월':
-            strn_dates[col+"("+(end_date - timedelta(days=30)).strftime('%Y-%m-%d')+")"] = (end_date - timedelta(days=30)).strftime('%Y-%m-%d')
-        elif col == '3개월':
-            strn_dates[col+"("+(end_date - timedelta(days=90)).strftime('%Y-%m-%d')+")"] = (end_date - timedelta(days=90)).strftime('%Y-%m-%d')
-        elif col == '6개월':
-            strn_dates[col+"("+(end_date - timedelta(days=180)).strftime('%Y-%m-%d')+")"] = (end_date - timedelta(days=180)).strftime('%Y-%m-%d')
-        elif col == '1년':
-            strn_dates[col+"("+(end_date - timedelta(days=365)).strftime('%Y-%m-%d')+")"] = (end_date - timedelta(days=365)).strftime('%Y-%m-%d')
-
-    return strn_dates
-
-#수익률 계산 함수, 공통으로 밀어넣어야함 ( 최대한 렌더링과 로직을 나눈다. )
-def cal_performance(df, target_funds, target_columns, strn_date :datetime, end_date:datetime):
-
-    
-    strn_dates = cal_strn_date(strn_date, end_date, target_columns)
-
-    fund_cond1 = df['펀드코드'].isin(target_funds)
-    strn_date_cond = df['일자'].isin(strn_dates.values())
-    end_date_cond = df['일자'].isin([end_date.strftime('%Y-%m-%d')]) 
-
-    strn_data = df.loc[fund_cond1 & strn_date_cond,['펀드코드','펀드명','일자','수정기준가']].rename(columns={'수정기준가':'시작_수정기준가'})
-    end_data = df.loc[fund_cond1 & end_date_cond,['펀드코드','일자','수정기준가']].rename(columns={'수정기준가':'끝_수정기준가'})
-    
-    #join
-    fund_data= pd.merge(strn_data,end_data,on='펀드코드',how='left').rename(columns={'일자_x':'시작_일자','일자_y':'끝_일자'})
-    fund_data['수익률'] = ((fund_data['끝_수정기준가'] / fund_data['시작_수정기준가'] - 1) * 100).round(2)
-    fund_data.sort_values(by=['펀드코드','시작_일자'],inplace=True)
-
-    #pivoting
-    performance_data = fund_data.pivot_table(index=['펀드코드','펀드명'],columns='시작_일자',values='수익률')
-    for colmn_name, date in strn_dates.items():
-        performance_data.rename(columns={date:colmn_name},inplace=True)
-        
-    return performance_data
+from common.rnrt_function import get_bm_data, cal_performance
 
 bm_code_list ={
     '코스피':'^KS11',
@@ -67,27 +20,20 @@ bm_name_list = {
     '^IXIC':'나스닥'
 }
 
-#BM 지수 가져는 함수, bm_code_list : BM 코드들의 리스트
-def get_bm_data(bm_code_list:list, strn_date, end_date)->pd.DataFrame:
-    
-    df_list = []
-    for bm_code in bm_code_list:
-        data = (yf.Ticker(bm_code).history(period="3Y")).reset_index()
-        data = data.rename(columns={'Date':'일자','Close':'종가'}).loc[:,['일자','종가']]        
-        date_cond = (data['일자'] >= strn_date.strftime('%Y-%m-%d')) & (data['일자'] <= end_date.strftime('%Y-%m-%d'))
-        data = data.loc[date_cond,:]
-        if len(data) > 0:
-            data.insert(0,'BM코드',bm_code)
-            data.insert(1,'BM명',bm_name_list[bm_code])
-            data['누적수익률'] = ((data['종가'] / data['종가'].iloc[0] - 1) * 100).round(2)
-            data['일자'] = pd.to_datetime(data['일자']).dt.strftime('%Y-%m-%d')
-            data = data[['BM코드','BM명','일자','누적수익률']]
-            df_list.append(data)
+# CSS 스타일 [공통으로 css 관리 필요함]
+st.markdown("""
+<style>
+    .block-container {
+        padding-left: 10rem;
+        padding-right: 10rem;
+        max-width: 100% !important;
+    }
+    h1 {
+        text-align: left !important;
+    }
+</style>
+""", unsafe_allow_html=True)
 
-    if len(df_list) > 0:
-        return pd.concat(df_list)
-    else:
-        return None
 
 # 챗봇 대화 초기화
 def clear_chat():
@@ -106,20 +52,6 @@ def load_data(format : str, file_name : str) -> pd.DataFrame:
     
 
 
-# CSS 스타일 [공통으로 css 관리 필요함]
-st.markdown("""
-<style>
-    .block-container {
-        padding-left: 10rem;
-        padding-right: 10rem;
-        max-width: 100% !important;
-    }
-    h1 {
-        text-align: left !important;
-    }
-</style>
-""", unsafe_allow_html=True)
-# 페이지 전체에 스타일 주입
 st.title("펀드성과분석")
 
 # 파일 업로드 & 수익률 계산 [ 유지보수를 위해 쪼개야 함 ]
